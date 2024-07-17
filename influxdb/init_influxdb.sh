54bee26caed52baedda6e21a1c0c1fb33b99a9c7
Kneatrum <martin.mwiti.n@gmail.com>
2024-07-17 06:17:18 +0300

    Updates to the influxdb initialization bash script

--- influxdb/init_influxdb.sh
+++ influxdb/init_influxdb.sh
@@ -1,17 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-set -a
-. ./back-end/.env
-set +a
 
-sleep 10
+# Access the environment variables
+USERNAME=${ADMIN_USERNAME}
+PASSWORD=${ADMIN_PASSWORD}
+ORG=${ORG_NAME}
+BUCKET=${BUCKET_NAME}
 
-influx setup --skip-verify --bucket fitbit --org kneatrum --username martin --password martin1234 --token "$API_TOKEN"  --force 
 
-# Add measurements
-measurements="sleep,idling,walking,jogging,biking,heart,steps,temperature"
-
-for measurement in ${measurements//,/ }
-do
-    influx write --bucket fitbit --org kneatrum --precision s --record $measurement
+# Wait for InfluxDB to start
+until curl -s http://influxdb:8086/health | grep "ready"; do
+    echo "Waiting for InfluxDB to start..."
+    sleep 1
 done
+
+
+influx setup \
+    --username "$USERNAME" \
+    --password "$PASSWORD" \
+    --org "$ORG" \
+    --bucket "$BUCKET" \
+    --retention 0 \
+    --force
+
+
+influx write \
+    --bucket "$BUCKET" \
+    --org "$ORG" \
+    --precision s \
+    "temperature,device=device_1 value=25"
