569fa554063e555042b8ddc69a6578f92bbc8c56
Kneatrum <martin.mwiti.n@gmail.com>
2024-07-31 06:42:19 +0300

    Method to add suffix to a date

--- front-end/src/context/ApiContext.js
+++ front-end/src/context/ApiContext.js
@@ -214,6 +214,21 @@ function findAverage(strArray){
 }
 
 
+
+function getOrdinalSuffix(day) {
+    if (day > 3 && day < 21) {
+        return 'th';
+    }
+    switch (day % 10) {
+        case 1: return 'st';
+        case 2: return 'nd';
+        case 3: return 'rd';
+        default: return 'th';
+    }
+}
+
+
+
 function setStepsData(stepsData){
     if(stepsData){
         dashboard.stepsData = stepsData;
