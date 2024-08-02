af927fa09b62346dd5afef7f27a334cdcb1dc53f
Kneatrum <martin.mwiti.n@gmail.com>
2024-08-02 19:58:03 +0300

    Adding a time scale to the bar chart

--- front-end/src/components/charts/Bar.js
+++ front-end/src/components/charts/Bar.js
@@ -1,24 +1,27 @@
 import {Bar} from 'react-chartjs-2'
 import { useContext, useEffect, useState } from 'react';
 import { ApiContext } from '../../context/ApiContext';
+// import 'chartjs-adapter-date-fns';
 
 import {
     Chart as ChartJS, 
-    CategoryScale, 
+    TimeScale, 
     LinearScale, 
     BarElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    PointElement
 } from 'chart.js'
 
 ChartJS.register(
-    CategoryScale, 
+    TimeScale, 
     LinearScale, 
     BarElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    PointElement
 );
 
 function BarChart(){
@@ -47,18 +50,23 @@ function BarChart(){
                     text: "Steps" // x-axis label
                 },
                 ticks: {
-                    stepSize: 1000
+                    stepSize: 100
                 }
             },
             x: {
+                type: 'time',
+                time: {
+                    unit: 'day'
+                },
                 title: {
                     display: true,
                     text: "Days" // y-axis label
                 }
             } 
         }
+        
     };
-
+    console.log(chartData)
     return( 
         <div className="card" style={{  borderRadius: '10px', marginTop: '20px', height: '390px' }}>
             <div className="card-header">
