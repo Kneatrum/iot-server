e67deca9dc9756e5fa6e2a0ffe2140dc88c27358
Kneatrum <martin.mwiti.n@gmail.com>
2024-08-02 19:58:54 +0300

    Adding time scale to line chart

--- front-end/src/components/charts/SleepingLine.js
+++ front-end/src/components/charts/SleepingLine.js
@@ -1,11 +1,14 @@
 import { Line } from 'react-chartjs-2'
 import { useContext, useState, useEffect } from 'react';
 import { ApiContext } from '../../context/ApiContext';
+import { Chart, registerables } from 'chart.js';
+import 'chartjs-adapter-date-fns';
+
 
 
 import {
     Chart as ChartJS, 
-    CategoryScale, 
+    TimeScale,
     LinearScale, 
     PointElement, 
     LineElement,
@@ -15,7 +18,7 @@ import {
 } from 'chart.js'
 
 ChartJS.register(
-    CategoryScale, 
+    TimeScale, 
     LinearScale, 
     PointElement, 
     LineElement,
@@ -24,8 +27,44 @@ ChartJS.register(
     Legend
 );
 
+Chart.register(...registerables);
 
 
+const flatLineWidthPlugin = {
+    id: 'flatLineWidthPlugin',
+    afterDatasetDraw: function(chart, args, options) {
+      const { ctx, data } = chart;
+      const meta = chart.getDatasetMeta(0); // Assuming you're working with the first dataset
+      const { data: points } = meta;
+      
+      
+      ctx.save();
+  
+      for (let i = 0; i < points.length - 1; i++) {
+        const currentPoint = points[i];
+        const nextPoint = points[i + 1];
+  
+        const deltaX = nextPoint.x - currentPoint.x;
+        const deltaY = nextPoint.y - currentPoint.y;
+  
+        // Check if the segment is flat
+        if (deltaY === 0) {
+          // Draw the flat line segment with increased thickness
+          ctx.beginPath();
+          ctx.lineWidth = options.flatLineWidth || 5; // Use the provided width or default to 5
+          ctx.strokeStyle = currentPoint.options.borderColor || options.borderColor || 'black';
+          ctx.moveTo(currentPoint.x, currentPoint.y);
+          ctx.lineTo(nextPoint.x, nextPoint.y);
+          ctx.stroke();
+          ctx.closePath();
+        }
+      }
+  
+      ctx.restore();
+    }
+  };
+
+Chart.register(flatLineWidthPlugin);
 
 
 function SleepingLine(){
@@ -38,11 +77,39 @@ function SleepingLine(){
         }
     }, [dashboardData]);
 
+    const data = {
+            labels: [
+                '2024-08-01T13:00:00', 
+                '2024-08-01T13:05:00', 
+
+                '2024-08-01T13:05:00', 
+                '2024-08-01T13:15:00',
+
+                '2024-08-01T13:15:00',
+                '2024-08-01T13:20:00',
+
+                '2024-08-01T13:20:00'
+            ],
+        datasets: [
+            {
+                label: "Time",
+                data: [ 3, 3, 2, 2, 1, 1, 0],
+                borderColor: 'orange'
+            },
+        ],
+    }
+
 
 
     const options = {
         responsive: true,
         plugins: {
+
+            flatLineWidthPlugin: {
+                flatLineWidth: 25 // Set the custom line width for flat segments
+                // borderColor: 'red' // Optional custom color for flat segments
+            },
+              
             legend: {
                 display: false,
                 position: "bottom",
@@ -50,6 +117,12 @@ function SleepingLine(){
         },
         scales: {
             x: {
+                type: 'time',
+                time: {
+                    unit: 'hour',
+                    stepSize: 1 // 24 hours (1 day)
+                },
+
                 // min: 0,
                 // max: 12,
                 display: true,
