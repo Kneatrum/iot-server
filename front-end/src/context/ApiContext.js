8fba5a72303e03f3ba60679f1d16208979522bea
Kneatrum <martin.mwiti.n@gmail.com>
2024-08-13 22:39:55 +0300

    Function no longer needed

--- front-end/src/context/ApiContext.js
+++ front-end/src/context/ApiContext.js
@@ -231,17 +231,7 @@ function findAverage(strArray){
 
 
 
-function getOrdinalSuffix(day) {
-    if (day > 3 && day < 21) {
-        return 'th';
-    }
-    switch (day % 10) {
-        case 1: return 'st';
-        case 2: return 'nd';
-        case 3: return 'rd';
-        default: return 'th';
-    }
-}
+
 
 
 
