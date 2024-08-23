ce17bcaf32dc6fc5e033302fd12977521ca4240a
Kneatrum <martin.mwiti.n@gmail.com>
2024-08-23 05:12:14 +0500

    Improvements to the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,28 @@ sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-c
 
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Get the hostname IP address
+HOST_URL=$(hostname -I)
+
+# Navigate to the ./back-end/ directory (ensure this directory exists)
+cd ./back-end/
+
+# Create the .env file and save the hostname IP address in it
+echo "HOST_URL=$HOST_URL" | sudo tee .env > /dev/null
+
+# Create a temporary user and password with an organization and a bucket.
+ADMIN_USERNAME=admin
+ADMIN_PASSWORD=adminpassword
+ORG_NAME=adminorg
+BUCKET_NAME=adminbucket
+
+echo "ADMIN_USERNAME=$ADMIN_USERNAME" | sudo tee -a .env > /dev/null
+echo "ADMIN_PASSWORD=$ADMIN_PASSWORD" | sudo tee -a .env > /dev/null
+echo "ORG_NAME=$ORG_NAME" | sudo tee -a .env > /dev/null
+echo "BUCKET_NAME=$BUCKET_NAME" | sudo tee -a .env > /dev/null
+
+# Go back to the root directory
+cd ../
+
+# Run the docker containers
 sudo docker-compose up
