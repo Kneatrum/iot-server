30e8e69a8df001d4d316dc06e74cb23c713c03d2
Kneatrum <martin.mwiti.n@gmail.com>
2024-09-04 23:15:39 +0300

    test

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { getSecret, createSecret } = require('./secrets/aws_secrets.js')
 const { setupInfluxDB } = require('./database/db_init.js');
-const {influxClient} = require('./database/influxdbClient.js');
+const influxClient = require('./database/influxdbClient.js');
 console.log("@@@@@@@@@@@@@@@@@")
 console.log("Bucket",influxClient.bucket);
 const cors = require('cors');
