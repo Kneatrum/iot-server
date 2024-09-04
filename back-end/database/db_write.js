3a0179891ef93e5d84c96ccd777b69d21110404d
Kneatrum <martin.mwiti.n@gmail.com>
2024-09-04 19:58:59 +0300

    Minor change

--- back-end/database/db_write.js
+++ back-end/database/db_write.js
@@ -1,6 +1,6 @@
 
 require('dotenv').config({ path: '../.env' });
-const { influxClient } = require('../influxdbClient');
+const { influxClient } = require('./influxdbClient');
 const { Point } = require('@influxdata/influxdb-client')
 
 
