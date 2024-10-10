34ca74d173b75c4ea989e3d7c0c2d6a0775e1559
Kneatrum <martin.mwiti.n@gmail.com>
2024-10-10 21:11:58 +0300

    Handle form submission during user registration

--- front-end/src/components/pages/Register.js
+++ front-end/src/components/pages/Register.js
@@ -1,34 +1,66 @@
 import React, { useState } from 'react';
-import styles from '../styles/auth.module.css'
+import styles from '../styles/auth.module.css';
+import api from '../../api/api';
+import { useNavigate } from 'react-router-dom';
 
 
 const Register = () => {
   const [formData, setFormData] = useState({
-    username: '',
+    userName: '',
     email: '',
     password: '',
   }); 
+
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Sign up data:', formData);
+    try {
+      
+      console.log('Sign up data:', formData);
+      const response = await api.post('/register', formData);
+  
+      if (response.status === 201) {
+        navigate('/login');
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 409) {
+        setErrorMessage(error.response.data.message);
+      } else {
+       
+        setErrorMessage('An error occurred. Please try again.');
+      }
+    } finally {
+      setLoading(false);
+    }
+    
   };
 
   return (
     <div className={styles.container}>
       <form className={styles.form} onSubmit={handleSubmit}>
         
+      {errorMessage && (
+          <div className={styles.errorMessage}>
+            {errorMessage}
+          </div>
+      )}
+
+
         <h1 className={styles.title}>Register</h1>
 
         <input
           type="text"
-          name="username"
+          name="userName"
           placeholder="Username"
-          value={formData.username}
+          value={formData.userName}
           onChange={handleChange}
           className={styles.input}
           required
@@ -54,6 +86,8 @@ const Register = () => {
           required
         />
         
+        {loading ? 'Logging in...' : ''}
+
         <button type="submit" className={styles.button}>
           Submit
         </button>
