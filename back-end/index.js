6472eff6d6c50f40add07c4ac1861ca3b8a2ac15
Kneatrum <martin.mwiti.n@gmail.com>
2025-02-19 20:57:34 +0300

    Add a namespace for users route

--- back-end/index.js
+++ back-end/index.js
@@ -298,8 +298,7 @@ async function startServer() {
 
 
         app.use("/", general_routes);
-        // app.use("/users", user_routes);
-        app.use("/", user_routes);
+        app.use("/users", user_routes);
         app.use("/ssl-services", sslServicesRoutes);
 
         sessionStore.sync();
