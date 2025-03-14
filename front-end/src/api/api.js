a3421597b91a07d06f507befc91b20e82c7b1995
Kneatrum <martin.mwiti.n@gmail.com>
2025-03-15 01:30:50 +0300

    Fix usersBaseUrl to include backend host for API calls

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -6,8 +6,8 @@ const backEndHost = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000'
 const certificatesRoute = '/ssl-services'
 const usersRoute = '/users';
 
-// const usersBaseUrl = `${backEndHost}${usersRoute}`
-const usersBaseUrl = `${usersRoute}`
+const usersBaseUrl = `${backEndHost}${usersRoute}`
+// const usersBaseUrl = `${usersRoute}`
 const certsBaseUrl = `${backEndHost}${certificatesRoute}`
 
 const api = axios.create({
