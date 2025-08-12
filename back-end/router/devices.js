ac38a3260d0fe254f70fcd464737f498fbc052af
Kneatrum <martin.mwiti.n@gmail.com>
2025-08-12 18:59:44 +0300

    Add a temporary log

--- back-end/router/devices.js
+++ back-end/router/devices.js
@@ -51,7 +51,8 @@ device_routes.post('/add-device', isAuthenticated, async (req, res) => {
             return res.status(500).json({ error: "Failed to add device" });
         }
         return res.status(201).json({ message: 'Device added successfully' });
-    } catch {
+    } catch (err) {
+        console.log("Error: ", err);
         // return res.status(500).json({ error: "Something went wrong" }); 
         return res.status(500).json({ error: err.message || 'Internal Server Error' });
     }
