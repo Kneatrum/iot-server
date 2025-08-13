86e8d33507350f932b927226ea32f6948d8cf265
Kneatrum <martin.mwiti.n@gmail.com>
2025-08-13 05:50:44 +0300

    Minor change

--- back-end/mqtt/mqtts-cert-generator.js
+++ back-end/mqtt/mqtts-cert-generator.js
@@ -46,19 +46,11 @@ async function getCredentials() {
         throw new Error("Failed to retrieve AWS secrets");
       }
       console.log("Successfully retrieved AWS secrets for MQTT certificates", results.data.mqtt_certs);
-      const { 
-        mqtt_ca_crt, 
-        mqtt_ca_key, 
-        mqtt_ca_password, 
-        mqtt_client_csr_subject, 
-      } = results.data.mqtt_certs;
+      const mqtt_ca_crt = results.data.mqtt_certs.mqtt_ca_crt;
+      const mqtt_ca_key = results.data.mqtt_certs.mqtt_ca_key;
+      const mqtt_ca_password =  results.data.mqtt_certs.mqtt_ca_password
+      const mqtt_client_csr_subject = results.data.mqtt_certs.mqtt_client_csr_subject;
 
-
-
-
-      console.log("mqtt_client_csr_subject:", mqtt_client_csr_subject);
-      console.log("mqtt_ca_crt:", mqtt_ca_crt);
-      console.log("mqtt_ca_key:", mqtt_ca_key);
       console.log("mqtt_ca_password:", mqtt_ca_password);
 
 
